c41d6e4bf36de1e8fc84e2ced5966db1af9d531c
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2022-02-11 13:14:09 +0100

    form input

--- src/Convert.jsx
+++ src/Convert.jsx
@@ -6,9 +6,11 @@ class Convert extends React.Component {
         super(props);
         this.datas = null;
         this.name = "TEST";
-        this.state = {pays: '1'};
+        this.state = {pays: '1',money: 0,year: 2010};
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChangePays = this.handleChangePays.bind(this);
+        this.handleChangeMoney = this.handleChangeMoney.bind(this);
+        this.handleChangeYear = this.handleChangeYear.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         try {
             this.datas = datafile;
@@ -22,12 +24,18 @@ class Convert extends React.Component {
     
     }
 
-    handleChange(event) {    
+    handleChangePays(event) {    
         this.setState({pays: event.target.value});  
+    }
+    handleChangeMoney(event) {    
+        this.setState({money: event.target.value});  
+    }
+    handleChangeYear(event) {    
+        this.setState({year: event.target.value});  
     }
     
     handleSubmit(event) {
-        alert('Pays choisi : '+this.state.pays);
+        alert('Pays choisi : '+this.state.pays+' Argent mis : '+this.state.money+' Année : '+this.state.year);
         event.preventDefault();
     }
 
@@ -37,10 +45,18 @@ class Convert extends React.Component {
         <h1>{this.name}</h1>
         <form onSubmit={this.handleSubmit}>
             <label>Selection pays :
-                <select value={this.state.pays} onChange={this.handleChange}>
+                <select value={this.state.pays} onChange={this.handleChangePays}>
                     {this.datas.map((data, i) =>
                     <option value={i}>{data.TIME}</option>)}
                 </select>
+            </label>
+            <label>
+            Argent:
+            <input type="number" value={this.state.money} onChange={this.handleChangeMoney} />        
+            </label>
+            <label>
+            Année:
+            <input type="number" value={this.state.year} onChange={this.handleChangeYear} min="2010" max="2021" />        
             </label>
             <input type="submit" value="Submit" />
         </form>
